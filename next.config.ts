1c5ed5fdf59565ba24e670ea949a04de94f9222b
Iryna Yeremizina <yeremizina1@gmail.com>
2026-01-02 22:11:54 +0200

    Final build check

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,37 @@
 // };
 
 // export default nextConfig;
+//
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     dangerouslyAllowLocalIP: true,
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "ac.goit.global",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+//
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://notehub-api.goit.study/api/:path*",
+      },
+    ];
+  },
   images: {
-    dangerouslyAllowLocalIP: true,
     remotePatterns: [
       {
-        protocol: "https",
         hostname: "ac.goit.global",
       },
     ],
