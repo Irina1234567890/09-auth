dbe8b5df5db41cf75f247500d850e57a32ceced2
Iryna Yeremizina <yeremizina1@gmail.com>
2026-01-02 22:44:37 +0200

    change 23-4

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -5,12 +5,35 @@
 //   baseURL,
 //   withCredentials: true,
 // });
+//
+//
+// import axios from "axios";
+
+// const host = process.env.NEXT_PUBLIC_API_URL || "";
+// const baseURL = host.endsWith("/") ? `${host}api` : `${host}/api`;
+
+// export const instance = axios.create({
+//   baseURL,
+//   withCredentials: true,
+// });
+//
+//
+// import axios from "axios";
+
+// const baseURL =
+//   typeof window !== "undefined"
+//     ? "/api"
+//     : (process.env.NEXT_PUBLIC_API_URL || "") + "/api";
+
+// export const instance = axios.create({
+//   baseURL: "/api",
+//   withCredentials: true,
+// });
+//
+//
 import axios from "axios";
 
-const host = process.env.NEXT_PUBLIC_API_URL || "";
-const baseURL = host.endsWith("/") ? `${host}api` : `${host}/api`;
-
 export const instance = axios.create({
-  baseURL,
+  baseURL: "/api",
   withCredentials: true,
 });
