0ff7b72d9c2ad1b2c80ce7852dabd4b8437e406a
Iryna Yeremizina <yeremizina1@gmail.com>
2026-01-02 22:29:58 +0200

    final

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,5 +1,15 @@
+// import axios from "axios";
+// const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
+
+// export const instance = axios.create({
+//   baseURL,
+//   withCredentials: true,
+// });
 import axios from "axios";
-const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
+
+// Використовуємо порожній рядок, якщо змінна не визначена, щоб уникнути "undefined/api"
+const host = process.env.NEXT_PUBLIC_API_URL || "";
+const baseURL = `${host}/api`;
 
 export const instance = axios.create({
   baseURL,
